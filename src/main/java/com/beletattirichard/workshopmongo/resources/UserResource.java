43aa1ac0fcfac7fa636a630670c3b4047c9c83ab
Richard Beletatti <richard.beletatti@gmail.com>
2020-07-18 11:06:39 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/beletattirichard/workshopmongo/resources/UserResource.java
+++ src/main/java/com/beletattirichard/workshopmongo/resources/UserResource.java
@@ -1,29 +1,27 @@
 package com.beletattirichard.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.beletattirichard.workshopmongo.domain.User;
+import com.beletattirichard.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll() {
-		User richard = new User(1, "Richard Beletatti", "richard.beletatti@gmail.com");
-		User bruno = new User(2, "Bruno Beletatti", "bruno.beletatti@gmail.com");
 
-		List<User> list = new ArrayList<>();
-
-		list.addAll(Arrays.asList(richard, bruno));
-
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
