315230f4727945c05237ad679c08ab6c680ee083
Richard Beletatti <richard.beletatti@gmail.com>
2020-07-20 10:52:36 -0300

    Atualização de usuário com PUT

--- src/main/java/com/beletattirichard/workshopmongo/resources/UserResource.java
+++ src/main/java/com/beletattirichard/workshopmongo/resources/UserResource.java
@@ -44,7 +44,6 @@ public class UserResource {
 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
 		User obj = service.fromDTO(objDTO);
 		obj = service.insert(obj);
-
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
@@ -55,4 +54,11 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+ 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 }
