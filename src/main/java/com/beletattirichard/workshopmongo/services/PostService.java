6d7084f3f702afb3ef6795ebd0f452d3ecc467ed
Richard Beletatti <richard.beletatti@gmail.com>
2020-07-20 17:35:03 -0300

    Obtendo um post por id

---
+++ src/main/java/com/beletattirichard/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.beletattirichard.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.beletattirichard.workshopmongo.domain.Post;
+import com.beletattirichard.workshopmongo.repositories.PostRepository;
+import com.beletattirichard.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
